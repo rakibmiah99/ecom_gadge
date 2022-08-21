ce04f255c8fe959545e9046649ae5800f66ebb66
Md. Rakib Miah <69368862+rakibmiah99@users.noreply.github.com>
2022-08-21 10:04:49 -0700

    Set Up

--- resources/views/components/home/offer-home.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="section pb_20 small_pt">
-    <div class="container">
-        <div id="OfferBannerListData" class="row">
-
-        </div>
-    </div>
-</div>
--- resources/views/pages/favList.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    @include('components.favList.fav-top')
-    <div class="main_content">
-        @include('components.favList.fav-list')
-        @include('layouts.newslatter')
-    </div>
-@endsection
